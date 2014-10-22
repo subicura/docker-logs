62c93e35ce09b58366e0545af27bc8a1187e94f6
Chungsub Kim <chungsub.kim@purpleworks.co.kr>
2014-10-22 16:28:19 +0900

    remove stdout log

--- diamond/run.sh
+++ diamond/run.sh
@@ -8,4 +8,4 @@ sed -i "s/__HOSTNAME__/${HOSTNAME}/g" /chroot/opt/diamond.conf
 sed -i "s/__GRAPHITE_HOST__/${GRAPHITE_HOST}/g" /chroot/opt/diamond.conf
 sed -i "s/__GRAPHITE_PORT__/${GRAPHITE_PORT}/g" /chroot/opt/diamond.conf
 
-chroot /chroot /usr/bin/diamond -f -c /opt/diamond.conf --skip-pidfile --skip-change-user -l
+chroot /chroot /usr/bin/diamond -f -c /opt/diamond.conf --skip-pidfile --skip-change-user
